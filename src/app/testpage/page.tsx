ffd42e5dceee3d19467031cd5f8a8b7d6feabf4c
Franco Valeriano <fgvaler@gmail.com>
2023-09-06 15:23:49 -0700

    Remove testpage entirely

--- src/app/testpage/page.tsx
+++
@@ -1,8 +0,0 @@
-
-// import { process_movesets } from '@/lib/process_raw_data';
-
-export default async function page(){
-    return <></>
-    // const data = await process_movesets();
-    // return <div className="whitespace-pre">{JSON.stringify(data, null, 2)}</div>
-};
